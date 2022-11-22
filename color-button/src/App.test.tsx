813849c6050cc4d20d58952f3b43720544c55cb3
San-Goon <tigerlove95@naver.com>
2022-11-22 11:25:23 +0900

    feat: make button clicked test

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,5 +9,10 @@ test('button has correct initial color', () => {
 });
 
 test('button turns blue when clicked', () => {
-
+  render(<App />);
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton.textContent).toBe('Change to red');
 });
