dbfe93239cc8991d2858bbf08a699f9ed7b2d7ff
San-Goon <tigerlove95@naver.com>
2022-11-22 11:12:34 +0900

    chore: make first test

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -3,12 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
-
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
 
-test('button has correct initial text', () => {
-
-});
 test('button turns blue when clicked', () => {
 
 });
