6d642a86201088f29bee71d34c7be1b0df9a84e5
San-Goon <tigerlove95@naver.com>
2022-11-22 11:46:05 +0900

    feat: make checkbox clicked event

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -16,3 +16,23 @@ test('button turns blue when clicked', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('initial conditions', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
+
+})
+
+test('checkbox test', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+  const checkBox = screen.getByRole('checkbox');
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeDisabled();
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeEnabled();
+})
--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -3,10 +3,18 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [isChecked, setIsChecked] = useState(false);
+
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
-      <button style={{backgroundColor: buttonColor}} onClick={() => {setButtonColor(newButtonColor)}}>Change to {newButtonColor}</button>
+      <button style={{backgroundColor: buttonColor}} onClick={() => {setButtonColor(newButtonColor)}} disabled={isChecked}>Change to {newButtonColor}</button>
+      <input
+        type='checkbox'
+        id="enable-button-checkbox"
+        defaultChecked={isChecked}
+        aria-checked={isChecked}
+        onChange={(e) => setIsChecked(e.target.checked)}/>
     </div>
   );
 }
